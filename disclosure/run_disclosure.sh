bbe9c6dd7301c4b728de2bd3a61a98e75534d42d
Rem01Gaming <Rem01_Gaming@proton.me>
2026-05-13 07:53:20 +0700

    disclosure: Discontinue CLI version
    This version has many false positives, user are strongly recommended to
    use latest APK release.
    
    Signed-off-by: Rem01Gaming <Rem01_Gaming@proton.me>

--- disclosure/run_disclosure.sh
+++ disclosure/run_disclosure.sh
@@ -3,42 +3,8 @@
 # Nothing scarry here ok?
 # you can read this script if you want
 
-abort_unsupported_arch() {
-	echo "*********************************************************"
-	echo "! Unsupported Architecture: $ARCH"
-	echo "! Your CPU architecture is not supported by Disclosure."
-	echo "*********************************************************"
-	exit 1
-}
-
-echo "Disclosure CLI is deprecated!"
-echo "You shouldn't use this anymore, instead use Disclosure APK."
+echo "Disclosure CLI is discontinued!"
+echo "You shouldn't use this anymore, as it contains many false positives."
+echo "Instead use Disclosure APK, you can get the Disclosure APK here:"
 echo ""
-
-# Move to home directory
-cd
-
-# Determine architecture
-ARCH=$(uname -m)
-case $ARCH in
-"aarch64") ARCH_TMP="arm64-v8a" ;;
-armv*) ARCH_TMP="armeabi-v7a" ;;
-*) abort_unsupported_arch ;;
-esac
-
-# Download disclosure
-curl -sO "https://dl.rem01gaming.dev/disclosure/$ARCH_TMP/disclosure"
-if [ ! -f ~/disclosure ]; then
-	echo "ERROR: Unable to download disclosure from GitHub pages, please check your Internet connection and try again."
-	exit 1
-fi
-
-# Execute disclosure
-chmod +x ./disclosure
-time ./disclosure
-
-# Remove disclosure back
-rm -f ./disclosure
-
-# Move back to previous directory
-cd - >/dev/null
+echo "https://t.me/disclosureofroot"
